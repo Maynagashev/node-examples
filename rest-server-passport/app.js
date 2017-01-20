21080cbe0e8e0a912124a06b8539410cd0a7c603
Maynagashev <lifesup@gmail.com>
2017-01-20 23:35:23 +0700

    adding ssl support

--- rest-server-passport/app.js
+++ rest-server-passport/app.js
@@ -44,6 +44,16 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Secure traffic only
+app.all('*', function(req, res, next){
+  console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
+  if (req.secure) {
+    return next();
+  };
+
+  res.redirect('https://'+req.hostname+':'+app.get('secPort')+req.url);
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
