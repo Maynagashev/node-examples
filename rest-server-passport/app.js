ebfd46b46c0bb874928d9172c6272b8996cfc101
Maynagashev <lifesup@gmail.com>
2017-01-23 11:54:18 +0700

    facebook auth

--- rest-server-passport/app.js
+++ rest-server-passport/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var authenticate = require('./authenticate');
 
 var config = require('./config');
 
@@ -26,6 +26,16 @@ var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
 
+// Secure traffic only
+app.all('*', function(req, res, next){
+  console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
+  if (req.secure) {
+    return next();
+  };
+
+  res.redirect('https://'+req.hostname+':'+app.get('secPort')+req.url);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -36,23 +46,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// passport config
-var User = require('./models/user');
-app.use(passport.initialize());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// Secure traffic only
-app.all('*', function(req, res, next){
-  console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
-  if (req.secure) {
-    return next();
-  };
-
-  res.redirect('https://'+req.hostname+':'+app.get('secPort')+req.url);
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '159855737836896',
+        clientSecret: 'b5ff7a616f0f38913bb5ee85f500d529',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
